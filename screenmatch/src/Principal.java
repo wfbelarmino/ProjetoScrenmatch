495b21d22d30ea395e931a3d8060ffc6e534939f
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2024-10-09 16:17:42 -0300

    Add files via upload

---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,20 @@
+import br.com.alura.screnmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme =  new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+
+
+    }
+}
